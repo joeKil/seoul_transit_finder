840bb84d314cb03bf3a3825a165e8ed65cbcc55d
solin <rhkrvkfk@hotmail.com>
2024-01-09 07:35:11 +0900

    updated: 메인스크린 경로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:seoul_transit_finder/presentation/main_screen.dart';
+import 'package:seoul_transit_finder/ui/main/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
