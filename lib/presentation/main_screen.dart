309d42d790f3bea2395bb02819a8c73935d13a12
solin <rhkrvkfk@hotmail.com>
2024-01-08 21:03:56 +0900

    feat: 전체데이터 불러오는 함수 추가

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:seoul_transit_finder/data/data_source/lost_item_api.dart';
+import 'package:seoul_transit_finder/data/model/lost_item_info.dart';
+import 'package:seoul_transit_finder/data/repository/load_item_repository_impl.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,39 +13,42 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   String searchQuery = '';
-  List<Map<String, String>> items = [
-    {
-      'name': 'shoes',
-      'registrationDate': '2021-02-02',
-      'collectionDate': '2021-02-03',
-      'status': '안좋음',
-      'type': '의류',
-      'storageLocation': '서울역'
-    },
-    {
-      'name': 'bag',
-      'registrationDate': '2021-03-05',
-      'collectionDate': '2021-03-10',
-      'status': '양호',
-      'type': '액세서리',
-      'storageLocation': '부산역'
-    },
-    {
-      'name': 'jacket',
-      'registrationDate': '2021-04-15',
-      'collectionDate': '2021-04-20',
-      'status': '매우 좋음',
-      'type': '의류',
-      'storageLocation': '대구역'
-    },
-  ];
+  List<LostItemInfo> items = [];
+
+  late final LostItemRepositoryImpl repository;
+
+  @override
+  void initState() {
+    super.initState();
+    repository =
+        LostItemRepositoryImpl(LostItemApi(dio: Dio())); // 여기서 LostItemApi 초기화
+    _loadData();
+  }
+
+  void _loadData() async {
+    try {
+      final result = await repository.getLostItems(''); // 전체 데이터 불러오기
+      print('Result: $result');
+      result.when(
+        success: (data) {
+          print('Data: $data');
+          setState(() {
+            items = data;
+          });
+        },
+        error: (e) => print('Error fetching data: $e'),
+      );
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, String>> filteredItems = items.where((item) {
-      return item['name']!.toLowerCase().contains(searchQuery.toLowerCase()) ||
-          item['registrationDate']!.contains(searchQuery) ||
-          item['collectionDate']!.contains(searchQuery);
+    List<LostItemInfo> filteredItems = items.where((item) {
+      return item.itemName.toLowerCase().contains(searchQuery.toLowerCase()) ||
+          item.registrationDate.contains(searchQuery) ||
+          item.foundDate.contains(searchQuery);
     }).toList();
 
     return Scaffold(
@@ -51,6 +58,7 @@ class _MainScreenState extends State<MainScreen> {
             setState(() {
               searchQuery = value;
             });
+            _loadData();
           },
           decoration: InputDecoration(
             hintText: '검색',
@@ -61,15 +69,15 @@ class _MainScreenState extends State<MainScreen> {
       body: ListView.builder(
         itemCount: filteredItems.length,
         itemBuilder: (context, index) {
-          var item = filteredItems[index];
+          final item = filteredItems[index];
           return Card(
             child: ListTile(
-              title: Text(item['name']!),
-              subtitle: Text('등록일자: ${item['registrationDate']}\n' +
-                  '수령일자: ${item['collectionDate']}\n' +
-                  '상태: ${item['status']}\n' +
-                  '종류: ${item['type']}\n' +
-                  '보관장소: ${item['storageLocation']}'),
+              title: Text(item.itemName),
+              subtitle: Text('등록일자: ${item.registrationDate}\n' +
+                  '수령일자: ${item.foundDate}\n' +
+                  '상태: ${item.lostItemStatus}\n' +
+                  '종류: ${item.lostItemType}\n' +
+                  '보관장소: ${item.storageLocation}'),
             ),
           );
         },
